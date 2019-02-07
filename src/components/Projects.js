b1e4fdd9198a21f13f961107d5cb0c56b25d8651
github username <odkasten@gmail.com>
2019-02-07 18:39:08 -0500

    basic projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import scheduler from "../schedulerScreenShot.png";
+import monologue from "../monologueScreenShot.png";
+import pong from "../pongScreenShot.png";
 
 export default class Projects extends Component {
   render() {
     return (
       <div className="pageHeaders">
         <h1>My Projects</h1>
+        <div className="projects">
+          <div className="project">
+            <Link to="/advscheduler">
+              <label>Adv.Scheduler</label>
+            </Link>
+            <img
+              className="screenshots"
+              src={scheduler}
+              alt="Adv. Scheduler screenshot"
+            />
+          </div>
+          <div className="project">
+            <Link to="monologuejournal">
+              <label>The Actor's Monologue Journal</label>
+            </Link>
+            <img
+              className="screenshots"
+              src={monologue}
+              alt="The Actor's Monologue Journal screenshot"
+            />
+          </div>
+          <div className="project">
+            <Link to="/pong">
+              <label>Pong</label>
+            </Link>
+            <br />
+            <img className="screenshots" src={pong} alt="Pong screenshot" />
+          </div>
+        </div>
       </div>
     );
   }
